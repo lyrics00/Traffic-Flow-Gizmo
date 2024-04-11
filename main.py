04220715e9fa69be0e2e01610009eac5f8f415ed
psuzuva <166549898+psuzuva@users.noreply.github.com>
2024-04-11 15:13:27 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+import sys
+from background.background_gizmo import background_setup
+from classes.car import Car
+import os
+
+pygame.init()
+screen, bg, clock = background_setup(pygame.display.set_mode((600, 480)), "./background.png")
+# Your main game logic goes here
+# For example:
+def run_game(screen, bg, clock):
+    pygame.mouse.set_visible(0)
+    pygame.display.set_caption('Gizmo')# fix indentation
+    c = Car("car1.png", 20, 20, 0, 4)
+    while True:
+        clock.tick(60)
+        screen.blit(bg, (0, 0))
+        x, y = pygame.mouse.get_pos()  
+        c.UpdateCoords(x)
+        c.Show(screen)  
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    sys.exit()    
+            pygame.display.update()
+run_game(screen, bg, clock)
