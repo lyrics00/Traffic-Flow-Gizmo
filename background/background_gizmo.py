218793339c4e3c8d9c48a16eab4ff16e65a9023c
ppt4pq <162641095+ppt4pq@users.noreply.github.com>
2024-04-21 15:15:50 -0400

    Update background_gizmo.py

--- background/background_gizmo.py
+++ background/background_gizmo.py
@@ -1,12 +1,27 @@
 import pygame
+import os
 
 def background_setup(screen, img_path):
     pygame.init()
-    screen = pygame.display.set_mode((1180, 820))
+    screen_info = pygame.display.Info()
+    screen_width, screen_height = screen_info.current_w, screen_info.current_h
+
+    screen = pygame.display.set_mode((1180, 800))
+    os.environ['SDL_VIDEO_WINDOW_POS'] = f"{(screen_width - 1180) // 2},{(screen_height - 800) // 2}"
+
     bg = pygame.image.load(img_path)
+    bg = pygame.transform.flip(bg, True, True)
+
+    screen_width, screen_height = screen.get_width(), screen.get_height()
+    bg_width, bg_height = bg.get_width(), bg.get_height()
+
+    bg_x = (screen_width - bg_width) // 2
+    bg_y = (screen_height - bg_height) // 2
 
     clock = pygame.time.Clock()
     pygame.mouse.set_visible(0)
     pygame.display.set_caption('Gizmo')
+
+    screen.blit(bg, (bg_x, bg_y))
 
     return screen, bg, clock
