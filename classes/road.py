2db027fd6365791d8b02a0e79e2f8537c9890ea0
Kuthan Kukrer <kuthan@kukrer.com>
2024-04-23 01:10:35 -0400

    some more updates

--- classes/road.py
+++ classes/road.py
@@ -1,6 +1,8 @@
 import pygame
+import time
+clock = pygame.time.Clock()
 class Lane(pygame.sprite.Sprite):
-    def __init__(self, lane_number: int, x_position: int, y_position: int, width: int, height: int, orientation: str, turn_direction='straight',):
+    def __init__(self, lane_number: int, x_position: int, y_position: int, width: int, height: int, orientation: str, restriction_time: float, turn_direction='straight'):
         super().__init__()
         self.x_position = x_position
         self.y_position = y_position
@@ -9,6 +11,15 @@ class Lane(pygame.sprite.Sprite):
         self.lane_number = lane_number
         self.orientation = orientation
         self.turn_direction = turn_direction
+        self.restriction_time = restriction_time
+        self.time = 0
+    def updateTimer(self, FPS):
+            self.time += clock.tick(FPS)
+    def TimeDone(self):
+        if self.time >= self.restriction_time:
+            self.time = 0
+            return True
+        return False
     def collidepoint(self, point_x, point_y):
         """
         Check if the given point (point_x, point_y) is within the rectangle defined by this Lane.
@@ -19,10 +30,13 @@ class Lane(pygame.sprite.Sprite):
             return False
     def draw_border(self, surface, border_color=(255, 0, 0), border_width=2):
         pygame.draw.rect(surface, border_color, (self.x_position, self.y_position, self.width, self.height), border_width)
+    def Delay(self, time_restriction):
+        self.elapsed_time = time.time()
+        return True
 
 # Define the Road class
 class Road(pygame.sprite.Sprite):
-    def __init__(self, num_lanes: int, lane_height: int, lane_width: int, road_x_position: int, road_y_position: int, lane_directions: list[str], orientation: str):
+    def __init__(self, num_lanes: int, lane_height: int, lane_width: int, road_x_position: int, road_y_position: int, lane_directions: list[str], orientation: str, restriction_time: float):
         super().__init__()
         # Light gray color for the road background
         self.num_lanes = num_lanes
@@ -32,6 +46,7 @@ class Road(pygame.sprite.Sprite):
         self.road_y_position = road_y_position
         self.lane_directions = lane_directions
         self.orientation = orientation  # Angle in degrees for the road's direction
+        self.restriction_time = restriction_time
         self.lanes = self.create_lanes()
     def getLanes(self):
         lane_sprites = self.lanes.sprites()  # Get a list of Lane sprites from the Group
@@ -45,31 +60,31 @@ class Road(pygame.sprite.Sprite):
         for i in range(self.num_lanes):
             if self.orientation == 'up':
                 if self.lane_directions[i] == 'straight-right' or self.lane_directions[i] == 'right' or self.lane_directions[i] == 'left' or self.lane_directions[i] == 'straight':
-                    lane = Lane(i, (i * self.lane_width) + self.road_x_position, self.road_y_position, self.lane_width, self.lane_height, self.orientation, self.lane_directions[i])
+                    lane = Lane(i, (i * self.lane_width) + self.road_x_position, self.road_y_position, self.lane_width, self.lane_height, self.orientation, self.restriction_time, self.lane_directions[i])
                     lanes.add(lane)
                 else:
-                    lane = Lane(i, (i * self.lane_width) + self.road_x_position, self.road_y_position, self.lane_width, self.lane_height, self.orientation, turn_direction="straight")
+                    lane = Lane(i, (i * self.lane_width) + self.road_x_position, self.road_y_position, self.lane_width, self.lane_height, self.orientation, self.restriction_time, turn_direction="straight")
                     lanes.add(lane)
             elif self.orientation == 'right':
                 if self.lane_directions[i] == 'straight-right' or self.lane_directions[i] == 'right' or self.lane_directions[i] == 'left' or self.lane_directions[i] == 'straight':
-                    lane = Lane(i, self.road_x_position, (i * self.lane_height) + self.road_y_position, self.lane_width, self.lane_height, self.orientation, self.lane_directions[i])
+                    lane = Lane(i, self.road_x_position, (i * self.lane_height) + self.road_y_position, self.lane_width, self.lane_height, self.orientation, self.restriction_time, self.lane_directions[i])
                     lanes.add(lane)
                 else:
-                    lane = Lane(i, self.road_x_position, (i * self.lane_height) + self.road_y_position, self.lane_width, self.lane_height, self.orientation, turn_direction="straight")
+                    lane = Lane(i, self.road_x_position, (i * self.lane_height) + self.road_y_position, self.lane_width, self.lane_height, self.orientation, self.restriction_time, turn_direction="straight")
                     lanes.add(lane)
             elif self.orientation == 'down':
                 if self.lane_directions[i] == 'straight-right' or self.lane_directions[i] == 'right' or self.lane_directions[i] == 'left' or self.lane_directions[i] == 'straight':
-                    lane = Lane(i, -(i * self.lane_width) + self.road_x_position, self.road_y_position, self.lane_width, self.lane_height, self.orientation, self.lane_directions[i])
+                    lane = Lane(i, -(i * self.lane_width) + self.road_x_position, self.road_y_position, self.lane_width, self.lane_height, self.orientation, self.restriction_time, self.lane_directions[i])
                     lanes.add(lane)
                 else:
-                    lane = Lane(i, -(i * self.lane_width) + self.road_x_position, self.road_y_position, self.lane_width, self.lane_height, self.orientation, turn_direction="straight")
+                    lane = Lane(i, -(i * self.lane_width) + self.road_x_position, self.road_y_position, self.lane_width, self.lane_height, self.orientation, self.restriction_time, turn_direction="straight")
                     lanes.add(lane)
             elif self.orientation == 'left':
                 if self.lane_directions[i] == 'straight-right' or self.lane_directions[i] == 'right' or self.lane_directions[i] == 'left' or self.lane_directions[i] == 'straight':
-                    lane = Lane(i, self.road_x_position, -(i * self.lane_height) + self.road_y_position, self.lane_width, self.lane_height, self.orientation, self.lane_directions[i])
+                    lane = Lane(i, self.road_x_position, -(i * self.lane_height) + self.road_y_position, self.lane_width, self.lane_height, self.orientation, self.restriction_time, self.lane_directions[i])
                     lanes.add(lane)
                 else:
-                    lane = Lane(i, self.road_x_position, -(i * self.lane_height) + self.road_y_position, self.lane_width, self.lane_height, self.orientation, turn_direction='straight')
+                    lane = Lane(i, self.road_x_position, -(i * self.lane_height) + self.road_y_position, self.lane_width, self.lane_height, self.orientation, self.restriction_time, turn_direction='straight')
                     lanes.add(lane)
         return lanes
 class Intersection:
--- main.py
+++ main.py
@@ -16,22 +16,23 @@ cimg1 = pygame.image.load("car1.png").convert_alpha()
 cimg1rect = cimg1.get_rect()
 cimg2 = pygame.image.load("car2.png").convert_alpha()
 cimg2rect = cimg1.get_rect()
-speed = 1.5
+SPEED = 1.5
+FPS = 60
 '''
     initialize()
     theoretically, we want a function that will initialize the screen with the background and the roads.
 '''
 # Your main game logic goes here
-# For example:
+# For example
 def run_game(screen, bg, img_x, img_y, clock):
     pygame.mouse.set_visible(0)
     pygame.display.set_caption('Gizmo')# fix indentation
     #lane width is 120 pixels
     # def __init__(self, num_lanes: int, lane_height: int, lane_width: int, road_x_position: int, road_y_position: int, lane_directions: list[str], orientation: str):
-    road1 = Road(num_lanes=3, lane_height=100, lane_width=130, road_x_position=210, road_y_position=-250, lane_directions=["straight", "straight", "straight"], orientation="down")
-    road2 = Road(num_lanes=1, lane_height=100, lane_width=130, road_x_position=1050, road_y_position=220, lane_directions=["straight"], orientation="left")
-    road3 = Road(num_lanes=3, lane_height=100, lane_width=100, road_x_position=440, road_y_position=800, lane_directions=["straight", "straight", "straight"], orientation="up")
-    road4 = Road(num_lanes=1, lane_height=100, lane_width=100, road_x_position=-300, road_y_position=160, lane_directions=["straight"], orientation="right")
+    road1 = Road(num_lanes=3, lane_height=100, lane_width=130, road_x_position=410, road_y_position=-250, lane_directions=["straight", "straight", "straight"], orientation="down", restriction_time= 1000.0)
+    road2 = Road(num_lanes=1, lane_height=100, lane_width=130, road_x_position=1050, road_y_position=220, lane_directions=["straight"], orientation="left", restriction_time= 2000.0)
+    road3 = Road(num_lanes=3, lane_height=100, lane_width=100, road_x_position=440, road_y_position=800, lane_directions=["straight", "straight", "straight"], orientation="up", restriction_time= 1000.0)
+    road4 = Road(num_lanes=1, lane_height=100, lane_width=100, road_x_position=-300, road_y_position=160, lane_directions=["straight"], orientation="right", restriction_time= 2000.0)
     # Assuming road1.lanes is a Group containing Lane sprites
     intersection1 = Intersection()
     intersection1.add_road(road1)
@@ -42,13 +43,13 @@ def run_game(screen, bg, img_x, img_y, clock):
 # Check if there are any Lane sprites in the list
     # def __init__ (self, imageFile, x, y, speed, intersection: Intersection, road: Road, lane_num: int):
     cars = []
-    c1 = Car(cimg1, speed=speed, intersection=intersection1, road=road1, lane_num=0)
+    c1 = Car(cimg1, speed=SPEED, intersection=intersection1, road=road1, lane_num=0)
     cars.append(c1)
-    c2 = Car(cimg2, speed=speed, intersection=intersection1, road=road2, lane_num=0)
+    c2 = Car(cimg2, speed=SPEED, intersection=intersection1, road=road2, lane_num=0)
     cars.append(c2)
-    c3 = Car(cimg1, speed=speed, intersection=intersection1, road=road3, lane_num=0)
+    c3 = Car(cimg1, speed=SPEED, intersection=intersection1, road=road3, lane_num=0)
     cars.append(c3)
-    c4 = Car(cimg2, speed=speed, intersection=intersection1, road=road4, lane_num=0)
+    c4 = Car(cimg2, speed=SPEED, intersection=intersection1, road=road4, lane_num=0)
     cars.append(c4)
     # Start all car threads
 
@@ -60,6 +61,7 @@ def run_game(screen, bg, img_x, img_y, clock):
         pygame.display.update()
         clock.tick(60)
         screen.blit(bg, (img_x, img_y))
+        '''
         c1.Show(screen)  
         c2.Show(screen)
         c3.Show(screen)
@@ -68,46 +70,48 @@ def run_game(screen, bg, img_x, img_y, clock):
         c2.Move()
         c3.Move()
         c4.Move()
-        '''
         update()
         Theoretically, we should just have a update function in the loop that organizes everything we want to update during each frame.
         '''
+        update(cars, intersection1)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                     sys.exit()   
             pygame.display.update(cimg1rect)
             pygame.display.update(cimg2rect)
-def update(list: list[Car]):
+def update(list: list[Car], intersection: Intersection):
      #This method should import random to have a certain probability of a random car being generated and starts moving, should store a list of all the cars
-     # list[cars] = GenerateCars(list)
-     ''' for i in len(list):
+    list= GenerateCars(list, intersection=intersection)
+
+    for i in list:
             i.Move()
             i.Show(screen)
-        cars = deleteCars(cars(a list))
-        we should also have a method that deletes cars if they are out of the screen.
-     '''
+    list = DeleteCars(list)
+        #we should also have a method that deletes cars if they are out of the screen.
 # TO DO: we should have a function that creates a car on the lane we want with a probabilistic image.
 def CreateCar(imageFile, speed, intersection: Intersection, road: Road, lane_num: int):
     image = pygame.image.load(imageFile).convert_alpha()
     newCar = Car(image, speed, intersection, road, lane_num)
     return newCar
 # TO DO: This method should import random to have a certain probability of a random car being generated and starts moving, should store a list of all the cars
-def GenerateCars(list: list[Car]):
-    num = random.random() * 100
-    '''if num < 1:
-        if num < 0.5:
+def GenerateCars(list: list[Car], intersection: Intersection):
+    num = random.random() * 10000
+    if num < 100:
+        if num < 50:
             image = "car1.png" 
-            list.append(CreateCar(image, speed, intersection, road, lane_num))
         else:
             image = "car2.png" 
-            list.append(CreateCar(image,  speed, intersection, road, lane_num))
-        '''
+        rand_road = random.choice(intersection.roads)
+        lane_num = random.randint(0, rand_road.num_lanes - 1)
+        lanes = rand_road.getLanes()
+        #if lanes[lane_num].TimeDone():
+        list.append(CreateCar(image, SPEED, intersection, rand_road, lane_num))
     return list
 # TO DO: we should also have a method that deletes cars if they are out of the screen.
 def DeleteCars(list: list[Car]):
     for car in list:
         [screen_x, screen_y] = screen.get_size()
-        if screen_x < car.x or -500 > car.x or car.y > screen_y or car.y < -500:
+        if screen_x + 500 < car.x or -500 > car.x or car.y > screen_y + 500 or car.y < -500:
             list.remove(car)
     return list
     
